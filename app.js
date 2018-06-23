1011da4ad6c5a12dad5c9cdba3caee63b4e47bf5
Thorgal <usa.rs@live.com>
2018-06-23 17:51:08 -0400

    create initial boilerplate

---
+++ app.js
@@ -0,0 +1,17 @@
+(() => {
+    const init = () => {
+        //DOM references
+        const newGameBtn = document.getElementById('newGameBtn');
+        const resetBtn = document.getElementById('resetBtn');
+        const easyBtn = document.getElementById('easyBtn');
+        const hardBtn = document.getElementById('hardBtn');
+        const scoreCount = document.getElementById('scoreCount'); 
+
+
+        const colorArray = ['red', 'blue', 'green'];
+
+        
+    };
+
+    document.addEventListener('DOMContentLoaded', init);
+})();
